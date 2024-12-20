c9aa756692ea80654df8558a31ed0389f2d48841
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2024-12-21 07:26:30 +0800

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,23 @@
-function App() {
+// src/App.jsx
+import React from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Achievements from './components/Achievements';
+import Footer from './components/Footer';
+
+const App = () => {
   return (
-    <div>
-      <h1>CV Saya</h1>
-      <nav>
-        <ul>
-          <li><a href="https://github.com/aidilsaputrakirsan/Pengajaran-Materi-Teori">Materi</a></li>
-          <li><a href="https://github.com/aidilsaputrakirsan/Pengajaran-Modul-Praktikum">Modul Praktikum</a></li>
-          <li><a href="https://github.com/aidilsaputrakirsan/Pengumpulan-Tugas-Pemeriksaan">Pengumpulan Tugas</a></li>
-        </ul>
-      </nav>
+    <div className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <Header />
+      <main className="container mx-auto px-4">
+        <About />
+        <Gallery />
+        <Achievements />
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
