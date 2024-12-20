ffd8614655e19f2bc53e06aeb4429e7976675cd7
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2024-12-21 06:54:27 +0800

    Deploy aplikasi React ke GitHub Pages

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>CV Saya</h1>
+      <nav>
+        <ul>
+          <li><a href="https://github.com/aidilsaputrakirsan/Pengajaran-Materi-Teori">Materi</a></li>
+          <li><a href="https://github.com/aidilsaputrakirsan/Pengajaran-Modul-Praktikum">Modul Praktikum</a></li>
+          <li><a href="https://github.com/aidilsaputrakirsan/Pengumpulan-Tugas-Pemeriksaan">Pengumpulan Tugas</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
